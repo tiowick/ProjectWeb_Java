f64cd729ac907a894a96bb423acce515146e4fbe
Jeferson sena <jefinho368@gmail.com>
2023-05-16 08:37:29 -0300

    Update of pom xml version 3.0.6, updating the imports

--- src/main/java/com/java_web/ProjectWeb/models/User.java
+++ src/main/java/com/java_web/ProjectWeb/models/User.java
@@ -1,17 +1,27 @@
 package com.java_web.ProjectWeb.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+// import javax.persistence.Column;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.Table;
+// import javax.validation.constraints.NotEmpty;
+// import javax.validation.constraints.NotNull;
+// import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = User.TABLE_NAME)
