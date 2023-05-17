693259d762e957121a0f0fdd41e0e6909506f905
Jeferson sena <jefinho368@gmail.com>
2023-05-17 15:14:41 -0300

    Create UserService

---
+++ src/main/java/com/java_web/ProjectWeb/services/UserService.java
@@ -0,0 +1,60 @@
+package com.java_web.ProjectWeb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.java_web.ProjectWeb.models.User;
+import com.java_web.ProjectWeb.repositories.TaskRepository;
+import com.java_web.ProjectWeb.repositories.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Long id){
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+            "Usuário não encontrado! Id: " + id + "Tipos " + User.class.getName()
+        ));
+    }
+
+    @Transactional
+    public User create(User obj){
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj){
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+
+    }
+
+    public void delete(Long id){
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possivel deletar pois há entidades relacionadas!");
+            // TODO: handle exception
+        }
+    }
+
+
+
+
+
+
+}
