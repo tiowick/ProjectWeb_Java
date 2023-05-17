e0eb3e7ecc49a4593b13d98bb326063968b0773e
Jeferson sena <jefinho368@gmail.com>
2023-05-17 15:39:17 -0300

    Refactor UserService

--- src/main/java/com/java_web/ProjectWeb/services/UserService.java
+++ src/main/java/com/java_web/ProjectWeb/services/UserService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.java_web.ProjectWeb.models.User;
-import com.java_web.ProjectWeb.repositories.TaskRepository;
+//import com.java_web.ProjectWeb.repositories.TaskRepository;
 import com.java_web.ProjectWeb.repositories.UserRepository;
 
 @Service
@@ -16,8 +16,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private TaskRepository taskRepository;
 
     public User findById(Long id){
         Optional<User> user = this.userRepository.findById(id);
@@ -30,7 +28,7 @@ public class UserService {
     public User create(User obj){
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
+        //this.taskRepository.saveAll(obj.getTasks());
         return obj;
     }
 
@@ -51,10 +49,6 @@ public class UserService {
             // TODO: handle exception
         }
     }
-
-
-
-
 
 
 }
