833334c87d208a9fbd8a373b566d94d5631e5671
Jeferson sena <jefinho368@gmail.com>
2023-05-30 11:16:07 -0300

    Create Webconfig

---
+++ src/main/java/com/java_web/ProjectWeb/configs/WebConfig.java
@@ -0,0 +1,15 @@
+package com.java_web.ProjectWeb.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+ 
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**"); //QUALQUER ROTA ACESSA (RECOMENTADO PARA DESENVOLVIMENTO)
+    }
+}
